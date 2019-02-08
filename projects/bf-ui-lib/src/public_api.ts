fd013af45c7142673e41cdedaddbeee7a7c859b4
joel <joel.barba@blueface.com>
2019-02-08 16:05:08 +0000

    Add bf-list-header-col to the public_api.ts

--- projects/bf-ui-lib/src/public_api.ts
+++ projects/bf-ui-lib/src/public_api.ts
@@ -7,3 +7,4 @@ export * from './lib/bf-ui-lib.component';
 export * from './lib/bf-ui-lib.module';
 
 export * from './lib/bf-btn/bf-btn.component';
+export * from './lib/bf-list-header-col/bf-list-header-col.component';
