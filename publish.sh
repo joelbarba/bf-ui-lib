568e3dd8cd59cd7a0e9bad1ae13a1a695f0b1ac9
joel <joel.barba@blueface.com>
2019-02-08 15:43:19 +0000

    Fix publish script

--- publish.sh
+++ publish.sh
@@ -1,4 +1,15 @@
 
+# Check git status
+isClean=`git status | grep "nothing to commit, working directory clean" | wc -l`
+if [ "$isClean" = "0" ]; then
+    echo ""
+    git status
+    echo ""
+    echo "ALERT!!! The repository is not clean. You should commit your last changes to set a label. Proceed anyway?"
+    read x
+fi
+
+# Versioning
 prevPkgVer=`cat projects/bf-ui-lib/package.json | grep version | cut -d"\"" -f 4`
 echo "Increment version:"
 npm run version_up
@@ -8,16 +19,18 @@ echo "Going from versioin $prevPkgVer --- to ---> $currPkgVer ?"
 echo "If that ok? If not, go to projects/bf-ui-lib/package.json and change it"
 read x
 
+# Packing
 echo ""
 echo "Generate library:"
 npm run pack_all
 
+# Publishing
 echo ""
 echo "Login into NPM register (as joel.blueface)"
 npm login
 
 pkgVer=`cat dist/bf-ui-lib/package.json | grep version | cut -d"\"" -f 4`
-pkgTar="./bf-ui-lib/dist/bf-ui-lib/bf-ui-lib-$pkgVer.tgz"
+pkgTar="./dist/bf-ui-lib/bf-ui-lib-$pkgVer.tgz"
 echo ""
-echo "Publish the library (version $pkgVer)"
+echo "Publish the library (version $pkgVer) Tar File: $pkgTar"
 npm publish $pkgTar
