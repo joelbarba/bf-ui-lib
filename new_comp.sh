2cdbc5624ee46b0101cca2b77041ccdb957e8629
joel <joel.barba@blueface.com>
2019-02-14 14:25:37 +0000

    Added documentation about how to add components

--- new_comp.sh
+++ new_comp.sh
@@ -23,18 +23,29 @@ read x
 
 echo ""
 ng generate component $ccName --project=bf-ui-lib
-ng generate component $wName --project=bf-ui-lib-sandbox
 
 # Injects component export from lib module: @NgModule({ exports: [ NewComp, ... ]
 libModFile="projects/bf-ui-lib/src/lib/bf-ui-lib.module.ts"
+echo ""
+echo "Export new component from lib modile ($libModFile):"
 awk -v compName="$CcName" '{ print $0 } { if ($1 == "exports:") { print "    " compName "Component,  // <--- New component" } }' $libModFile > temp.tmp 
 mv temp.tmp $libModFile
-
-echo "New component exported from lib modile ($libModFile):"
-echo ""
 # cat $libModFile
 # echo ""
 
+# Export the new component from public_api.ts
+echo ""
+echo "Adding export in public_api.ts"
+apiExportFile="projects/bf-ui-lib/src/public_api.ts"
+cat $apiExportFile > temp.tmp
+echo "export * from './lib/"$vName"/"$vName".component';" >> temp.tmp
+mv temp.tmp $apiExportFile
+
+
+
+ # ------ Demo Wrapper into Sandbox: ------------
+
+ng generate component $wName --project=bf-ui-lib-sandbox
 
 # Add template to the HTML demo wrapper of the sandbox (-demo.component.html)
 wViewFile="projects/bf-ui-lib-sandbox/src/app/"$vName"-demo/"$vName"-demo.component.html"
@@ -62,13 +73,7 @@ awk -v CcName="$CcName" -v vName="$vName" -v q="'" '{
 mv temp.tmp $libServiceFile
 
 
-# Export the new component from public_api.ts
-echo ""
-echo "Adding export in public_api.ts"
-apiExportFile="projects/bf-ui-lib/src/public_api.ts"
-cat $apiExportFile > temp.tmp
-echo "export * from './lib/"$vName"/"$vName".component';" >> temp.tmp
-mv temp.tmp $apiExportFile
+
 
 echo ""
 echo ""
