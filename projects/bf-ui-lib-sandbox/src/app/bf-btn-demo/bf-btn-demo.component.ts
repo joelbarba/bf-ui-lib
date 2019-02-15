6eb1b8f519122fc46fe4d03f256b312fde053c60
joel <joel.barba@blueface.com>
2019-02-15 16:22:02 +0000

    bf-btn - Added disabled and encapsulated CSS with color resete mixin

--- projects/bf-ui-lib-sandbox/src/app/bf-btn-demo/bf-btn-demo.component.ts
+++ projects/bf-ui-lib-sandbox/src/app/bf-btn-demo/bf-btn-demo.component.ts
@@ -12,28 +12,63 @@ export class BfBtnDemoComponent implements OnInit {
   public api = BfBtnDoc.api;
   public instance = BfBtnDoc.instance;
 
-  public instance2 = 
-`<bf-btn [bfText]="'Save User'" (click)="myFunc()">
-</bf-btn>`;
+  public instance2 = `<bf-btn bfText="Add User"  (onClick)="myFunc($event)" [bfDisabled]="false"></bf-btn>
+<bf-btn bfText="Save User" (onClick)="myFunc($event)" [bfDisabled]="true"></bf-btn>`;
+  public instance3 = `<bf-btn bfText="Add User" bfIcon="icon-eye" (onClick)="myFunc($event)"></bf-btn>`;
+  public instance4 = `<bf-btn bfType="save"    bfText="Save User"   (onClick)="myFunc($event)"></bf-btn>
+<bf-btn bfType="add"     bfText="Add User"    (onClick)="myFunc($event)"></bf-btn>
+<bf-btn bfType="delete"  bfText="Delete User" (onClick)="myFunc($event)"></bf-btn>
+<bf-btn bfType="cancel"  bfText="Cancel"      (onClick)="myFunc($event)"></bf-btn>
+<bf-btn bfType="expand"></bf-btn>
+<bf-btn bfType="collapse"></bf-btn>
+`;
+  public instance5 = `<bf-btn bfText="Primary"    bfType="primary"    (onClick)="myFunc($event)"></bf-btn>
+<bf-btn bfText="Secondary"  bfType="secondary"  (onClick)="myFunc($event)"></bf-btn>
+<bf-btn bfText="Tertiary"   bfType="tertiary"   (onClick)="myFunc($event)"></bf-btn>
+<bf-btn bfText="Quaternary" bfType="quaternary" (onClick)="myFunc($event)"></bf-btn>
+<bf-btn bfText="Warning"    bfType="warning"    (onClick)="myFunc($event)"></bf-btn>
+<bf-btn bfText="Extra"      bfType="extra"      (onClick)="myFunc($event)"></bf-btn>`
 
-  public instance3 = 
-`<bf-btn [bfText]="'Add User'" [bfIcon]="'icon-plus'"
-        (click)="myFunc()">
-</bf-btn>`;
+public cssReset =
+`$bf-colors: (
+  "primary"     : $primary_color,
+  "secondary"   : $secondary_color,  
+  "tertiary"    : $tertiary_color,   
+  "quaternary"  : $quaternary_color, 
+  "warning"     : $warning_color,    
+  "extra"       : $extra_color,      
+  "white"       : $white,
+);          
+
+// Button color reset
+@mixin btn-color-mixin($btn-color) {
+  background: $btn-color;
+  .btn-icon-section { background: darken($btn-color, 5%); }
+  &:hover:not(:disabled) {
+    background: darken($btn-color, 3%);
+    .btn-icon-section { background: darken($btn-color, 7%); }
+  }
+}
+@each $color, $value in $bf-colors { .#{$color}.bf-btn  { @include btn-color-mixin($value); } }`;
+
 
   constructor() { }
-
-  ngOnInit() {
-  }
-
+  ngOnInit() { }
+  public count = '';
 }
+
+
+
 
 
 export const BfBtnDoc = {
   name    : `bf-btn`,
   desc    : `Generates a button.`, 
-  api     : `[bfText]: Text of the button
-[bfIcon]: Icon of the button (icomoon class)`,
-  instance: `<bf-btn></bf-btn>`, 
+  api     : `[bfText]     : Text of the button
+[bfType]     : Class of the button [primary, secondary, tertiary, quaternary, warning, extra] or predefined type [add, save, edit, delete, cancel, expand, collapse]
+[bfDisabled] : True=Button is disabled, False=Enabled
+[bfIcon]     : Icon of the button (icomoon class)
+(onClick)    : Click event handler`,
+  instance: `<bf-btn bfType="edit" (onClick)="myFunc($event)"></bf-btn>`, 
   demoComp: BfBtnDemoComponent
 }
--- projects/bf-ui-lib/src/lib/bf-btn/bf-btn.component.ts
+++ projects/bf-ui-lib/src/lib/bf-btn/bf-btn.component.ts
@@ -4,16 +4,29 @@ import { Component, OnInit, Input, Output, EventEmitter, ViewEncapsulation } fro
   selector: 'bf-btn',
   templateUrl: './bf-btn.component.html',
   styleUrls: ['./bf-btn.component.scss'],
-  encapsulation: ViewEncapsulation.None
+  // encapsulation: ViewEncapsulation.None
 })
 export class BfBtnComponent implements OnInit {
+  @Output() onClick = new EventEmitter<any>();
   @Input() bfText: string = '';
+  @Input() bfType: string = ''; // save, update, add, delete, cancel
   @Input() bfIcon: string = 'icon-arrow-right3';
+  @Input() bfDisabled: boolean = false;
+  public btnClass: string = 'primary';
 
   constructor() { }
 
   ngOnInit() {
-    // console.log('ngOnInit');
+    // Common predefined type
+    if (!!this.bfType) { this.btnClass = this.bfType; }
+    if (this.bfType === 'edit')     { this.btnClass = 'primary';   this.bfIcon = 'icon-pencil'; }
+    if (this.bfType === 'save')     { this.btnClass = 'primary';   this.bfIcon = 'icon-arrow-right3'; }
+    if (this.bfType === 'update')   { this.btnClass = 'primary';   this.bfIcon = 'icon-arrow-right3'; }
+    if (this.bfType === 'add')      { this.btnClass = 'primary';   this.bfIcon = 'icon-plus'; }
+    if (this.bfType === 'delete')   { this.btnClass = 'tertiary';  this.bfIcon = 'icon-cross'; }
+    if (this.bfType === 'cancel')   { this.btnClass = 'secondary'; this.bfIcon = 'icon-blocked'; }
+    if (this.bfType === 'expand')   { this.btnClass = 'secondary'; this.bfIcon = 'icon-arrow-down3'; }
+    if (this.bfType === 'collapse') { this.btnClass = 'secondary'; this.bfIcon = 'icon-arrow-up3'; }
   }
   
   ngOnChanges() {
