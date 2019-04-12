8927ec08e46d8783a20ca12bfdf6a882bb635816
joel <joel.barba@blueface.com>
2019-04-12 17:15:39 +0100

    bf-confirm Fixed exported api

--- projects/bf-ui-lib/src/public_api.ts
+++ projects/bf-ui-lib/src/public_api.ts
@@ -9,6 +9,7 @@ export * from './lib/abstract-translate.service';
 export * from './lib/bf-growl.module';
 export * from './lib/bf-growl/bf-growl.service';
 export * from './lib/bf-growl/bf-growl-pop-up.component';
+export * from './lib/bf-confirm/bf-confirm.service';
 
 export * from './lib/bf-btn/bf-btn.component';
 export * from './lib/bf-list-header-col/bf-list-header-col.component';
@@ -19,4 +20,3 @@ export * from './lib/bf-input/bf-input.component';
 export * from './lib/bf-dropdown/bf-dropdown.component';
 export * from './lib/bf-switch/bf-switch.component';
 export * from './lib/bf-quantity/bf-quantity.component';
-export * from './lib/bf-confirm/bf-confirm.component';
