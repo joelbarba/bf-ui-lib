e209192701656271e701f4da6ea8e3dfadfa9ca0
joel <joel.barba@blueface.com>
2019-05-12 21:54:27 +0100

    Added bf-switch

--- projects/bf-ui-lib-sandbox/src/app/bf-switch-demo/bf-switch-demo.component.ts
+++ projects/bf-ui-lib-sandbox/src/app/bf-switch-demo/bf-switch-demo.component.ts
@@ -14,9 +14,101 @@ export class BfSwitchDemoComponent implements OnInit {
   public desc = BfSwitchDoc.desc;
   public api = BfSwitchDoc.api;
   public instance = BfSwitchDoc.instance;
+  public myValue = true;
+  public myVal = false;
 
   public instance2 = 
-`<bf-switch</bf-switch>`;
+`<bf-switch [(ngModel)]="myVal" [bfDisabled]="true"></bf-switch>`;
+
+  public instance3 =
+`<bf-switch [(ngModel)]="myVal" bfLabel="view.common.name" bfLabelPos="left"></bf-switch>`;
+
+  public instance4 =
+`<bf-switch [(ngModel)]="myVal" bfLabel="view.common.name"></bf-switch>`;
+
+  public cssReset = `div.bf-switch {
+
+  .switch-bar {  // Background bar
+    background: white;
+    border-color: #ccc;
+  }
+
+  .switch-value {  // Text color (ON/OFF)
+    .bf-switch-off-text { color: red; }
+    .bf-switch-on-text  { color: green; }
+  }
+
+  .switch-lever {  // Switch color
+    color: white;
+    background: #f3f3f3;
+    &.is-on  { background: $primary_color; }
+    &.is-off { background: red; }
+  }
+
+  &.is-disabled {
+    .switch-bar   { background: #d4d4d4; }
+    .switch-lever { background: #f3f3f3; }
+  }
+}`;
+
+  public brStr = `
+`;
+  public bsStr = `
+           `;
+  public swTooltipPos = [
+    { id: 'top',        text: 'top'    },
+    { id: 'right',      text: 'right'  },
+    { id: 'bottom',     text: 'bottom' },
+    { id: 'left',       text: 'left'   },
+  ];
+  public swTooltipBody = [
+    { id: 'true',       text: 'true'   },
+    { id: 'false',      text: 'false'  },
+  ];
+  public swCode = ``;
+  public swConf = {
+    hasLabel: false, labelText: 'Feature Enabled', labelPosLeft: false,
+    hasTooltip: false, btnTooltip: 'Hello World', btnTooltipPos: null, btnTooltipBody: false,
+    isDisabled: false,
+    hasOnText: false, hasOffText: false, onText: 'Yes', offText: 'No'
+  };
+
+  public updateCustomSw = () => {
+    this.swCode = `<bf-switch [(ngModel)]="myValue"`;
+
+    if (this.swConf.hasLabel) {
+      this.swCode += this.bsStr + ` bfLabel="${this.swConf.labelText}"`;
+
+      if (this.swConf.labelPosLeft) {
+        this.swCode += this.bsStr + ` bfLabelPos="left"`;
+      }
+    }
+
+    if (this.swConf.hasOnText) {
+      this.swCode += this.bsStr + ` bfOnText="${this.swConf.onText}"`;
+    }
+    if (this.swConf.hasOffText) {
+      this.swCode += this.bsStr + ` bfOffText="${this.swConf.offText}"`;
+    }
+
+    if (this.swConf.isDisabled) {
+      this.swCode += this.bsStr + `[bfDisabled]="true"`;
+    }
+
+    if (this.swConf.hasTooltip) {
+      this.swCode += this.bsStr + ` bfTooltip="${this.swConf.btnTooltip}"`;
+      if (!!this.swConf.btnTooltipPos) {
+        this.swCode += this.bsStr + ` bfTooltipPos="${this.swConf.btnTooltipPos}"`;
+      }
+      if (!!this.swConf.btnTooltipBody) {
+        this.swCode += this.bsStr + ` bfTooltipBody="${this.swConf.btnTooltipBody}"`;
+      }
+
+    }
+
+    this.swCode += (`>` + this.brStr + `</bf-btn>`);
+  };
+
 
   constructor() { }
 
@@ -27,8 +119,16 @@ export class BfSwitchDemoComponent implements OnInit {
 
 export const BfSwitchDoc = {
   name    : `bf-switch`,
-  desc    : `Generates a button.`, 
-  api     : `[bfText]: Button text`,
-  instance: `<bf-switch></bf-switch>`, 
+  desc    : `Generates a switch.`,
+  api     : `[(ngModel)]     : The ngModel directive is linked to the inner <input>, so that can be used as a form element with ngForm (status is propagated)
+[bfDisabled]    : Whether the switch is disabled (true) or not (false).
+[bfOnText]      : Text displayed when value is true. By default = ON.
+[bfOffText]     : Text displayed when value is false. By default = OFF.
+[bfLabel]       : If text provided, a label will be added.
+[bfLabelPos]    : Position of the label (top | left). By default = top.
+[bfTooltip]     : Tooltip on the label
+[bfTooltipPos]  : Position of the tooltip on the label
+[bfTooltipBody] : Whether the tooltip on the label is append on the body (true) or not (false)`,
+  instance: `<bf-switch [(ngModel)]="myVal"></bf-switch>`,
   demoComp: BfSwitchDemoComponent
 };
--- projects/bf-ui-lib/src/lib/bf-switch/bf-switch.component.ts
+++ projects/bf-ui-lib/src/lib/bf-switch/bf-switch.component.ts
@@ -1,15 +1,57 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, EventEmitter, forwardRef, Input, OnInit, Output} from '@angular/core';
+import {ControlValueAccessor, NG_VALUE_ACCESSOR} from "@angular/forms";
 
 @Component({
   selector: 'bf-switch',
   templateUrl: './bf-switch.component.html',
-  styleUrls: ['./bf-switch.component.scss']
+  styleUrls: ['./bf-switch.component.scss'],
+  providers: [
+    {
+      provide: NG_VALUE_ACCESSOR, multi: true,
+      useExisting: forwardRef(() => BfSwitchComponent),
+    },
+  ]
 })
-export class BfSwitchComponent implements OnInit {
+export class BfSwitchComponent implements ControlValueAccessor {
+
+  // @Output() bfClick = new EventEmitter<any>();
+  @Input() bfDisabled: boolean = false;
+  @Input() bfOnText: string = 'ON';
+  @Input() bfOffText: string = 'OFF';
+
+  @Input() bfLabel: string;
+  @Input() bfLabelPos: 'top' | 'left' = 'top';
+  @Input() bfTooltip: string;
+  @Input() bfTooltipPos: string = 'top';
+  @Input() bfTooltipBody: boolean;
+
+  public bfModel = false; // Internal holding of the ngModel
 
   constructor() { }
 
+  // ------- ControlValueAccessor -----
+
+  // ControlValueAccessor --> writes a new value from the form model into the view
+  writeValue(value: any) {
+    if (value !== undefined) {
+      this.bfModel = !!value;
+    }
+  }
+  public propagateModelUp = (_:boolean) => {}; // This is just to avoid type error (it's overwritten on register)
+  registerOnChange(fn) { this.propagateModelUp = fn; }
+  registerOnTouched(fn) { }
+
+  // ----------------
+
+
   ngOnInit() {
+  }
+
+  public onSwitch = () => {
+    if (!this.bfDisabled) {
+      this.bfModel = !this.bfModel;
+      this.propagateModelUp(this.bfModel);
+    }
   }
 
 }
