39e6150f84de253feaf5940e3aa5adbe8ef636f9
joel <joel.barba@blueface.com>
2019-06-10 18:56:52 +0100

    Added <bf-textarea>

--- projects/bf-ui-lib-sandbox/src/app/bf-textarea-demo/bf-textarea-demo.component.ts
+++ projects/bf-ui-lib-sandbox/src/app/bf-textarea-demo/bf-textarea-demo.component.ts
@@ -14,9 +14,57 @@ export class BfTextareaDemoComponent implements OnInit {
   public desc = BfTextareaDoc.desc;
   public api = BfTextareaDoc.api;
   public instance = BfTextareaDoc.instance;
+  public instance2 = `<bf-textarea</bf-textarea>`;
+  public myVal = '';
 
-  public instance2 = 
-`<bf-textarea</bf-textarea>`;
+  public brStr = `\n`;
+  public bsStr = `\n             `;
+  public customCompCode = `<bf-dropdown [(ngModel)]="selObj" [bfList]="myList"></bf-dropdown>`;
+  public compConf:any = {
+    isRequired: false,
+    isDisabled: false,
+    rows: null,
+    hasLabel: false,   labelText: 'My Description',
+    hasTooltip: false, tooltipText: 'Hello World', tooltipPos: null, tooltipBody: false,
+  };
+  public upComp = () => {
+    this.customCompCode = `<bf-textarea `;
+
+    // let compClasses = '';
+    // if (this.compConf.hasFullWidth) { compClasses = 'full-width'; }
+    // // if (this.compConf.hasSquash) {
+    // //   if (!!compClasses) { compClasses += ' '; }
+    // //   compClasses += 'squash';
+    // // }
+    // if (!!compClasses) {
+    //   this.customDropdownCode += `class="${compClasses}"` + this.bsStr;
+    // }
+    this.customCompCode += `[(ngModel)]="myVal"` + this.bsStr;
+    this.customCompCode += `(ngModelChange)="doSomething($event)"`;
+
+    if (this.compConf.hasLabel) {
+      this.customCompCode += this.bsStr + `bfLabel="${this.compConf.labelText}"`;
+    }
+    if (this.compConf.isRequired) {
+      this.customCompCode += this.bsStr + `[bfRequired]="true"`;
+    }
+    if (this.compConf.isDisabled) {
+      this.customCompCode += this.bsStr + `[bfDisabled]="true"`;
+    }
+
+
+    if (this.compConf.hasTooltip) {
+      this.customCompCode += this.bsStr + `bfTooltip="${this.compConf.tooltipText}"`;
+      if (!!this.compConf.tooltipPos) {
+        this.customCompCode += this.bsStr + `bfTooltipPos="${this.compConf.tooltipPos}"`;
+      }
+      if (!!this.compConf.tooltipBody) {
+        this.customCompCode += this.bsStr + `bfTooltipBody="${this.compConf.tooltipBody}"`;
+      }
+    }
+
+    this.customCompCode += (`>` + this.brStr + `</bf-textarea>`);
+  };
 
   constructor() { }
 
@@ -27,8 +75,16 @@ export class BfTextareaDemoComponent implements OnInit {
 
 export const BfTextareaDoc = {
   name    : `bf-textarea`,
-  desc    : `Generates a button.`, 
-  api     : `[bfText]: Button text`,
-  instance: `<bf-textarea></bf-textarea>`, 
+  desc    : `Generates a textarea component.`,
+  api     : `[(ngModel)]     : The ngModel directive is linked to the inner <textarea>, so that can be used as a form element with ngForm (status is propagated).
+[bfRows]        : Number of rows of the <textarea> (4 by default).
+[bfLabel]       : Label of the input (automatically translated). If not provided, no label is displayed.
+[bfRequired]    : Whether the input is required or not
+[bfDisabled]    : Whether the input is disabled or not
+[bfPlaceholder] : Placeholder text (automatically translated)
+[bfTooltip]     : If label provided, adds a info badge with a tooltip (automatically translated)
+[bfTooltipPos]  : Position of the tooltip (top by default)
+[bfTooltipBody] : Whether the tooltip is append to the body (default true) or next the the html element (false). The parent contaniner may affect the visibility of the toolt`,
+  instance: `<bf-textarea [(ngModel)]="myVal" bfLabel="My Text"></bf-textarea>`,
   demoComp: BfTextareaDemoComponent
 };
--- projects/bf-ui-lib/src/lib/bf-textarea/bf-textarea.component.ts
+++ projects/bf-ui-lib/src/lib/bf-textarea/bf-textarea.component.ts
@@ -1,15 +1,158 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Input, Output, EventEmitter, Inject, forwardRef } from '@angular/core';
+import { ViewChild, ElementRef } from '@angular/core';
+import { FormControl, ControlValueAccessor, Validators, NG_VALUE_ACCESSOR, NG_VALIDATORS } from '@angular/forms';
+import { AbstractTranslateService } from '../abstract-translate.service';
+import { NgbPopoverConfig } from '@ng-bootstrap/ng-bootstrap';
+import * as Rx from 'rxjs';
+import * as RxOp from 'rxjs/operators';
 
 @Component({
   selector: 'bf-textarea',
   templateUrl: './bf-textarea.component.html',
-  styleUrls: ['./bf-textarea.component.scss']
+  styleUrls: ['./bf-textarea.component.scss'],
+  providers: [
+    {
+      provide: NG_VALUE_ACCESSOR, multi: true,
+      useExisting: forwardRef(() => BfTextareaComponent),
+    },
+    { // Custom validator
+      provide: NG_VALIDATORS, multi: true,
+      useExisting: forwardRef(() => BfTextareaComponent),
+    }
+  ]
 })
 export class BfTextareaComponent implements OnInit {
+// export class BfInputComponent implements OnInit {
+  // @Output() bfModelChange = new EventEmitter<string>();
+  // @Input() bfModel: string;
 
-  constructor() { }
+  public bfModel: string; // Internal to hold the linked ngModel on the wrapper
 
-  ngOnInit() {
+  @Input() bfLabel: string = '';
+  @Input() bfRequired: boolean = false;
+  @Input() bfDisabled: boolean = false;
+  @Input() bfRows = 4;
+  @Input() bfPlaceholder: string = '';
+
+  @Input() bfTooltip    : string = '';
+  @Input() bfTooltipPos : string = 'top';
+  @Input() bfTooltipBody : boolean = true;
+
+  @Input() bfErrorPos: string = 'top-right';  // top-right, bottom-left, bottom-right
+
+
+
+  public status : string = 'pristine';      // pristine, valid, error, loading
+
+  public bfLabelTrans: string = '';         // Translated text for the label
+  public bfTooltipTrans: string = '';       // Translated text for the tooltip of the label
+
+  public bfPlaceholderTrans: string = '';   // Translated text for the placeholder of the input
+  public errorPosition: string = 'top-right';
+  public errorText: string = 'Invalid value';
+
+  @ViewChild('ngInputRef') ngInputRef: ElementRef;
+  public inputCtrl:FormControl; // <-- ngInputRef.control
+
+  constructor(
+    @Inject('TranslateService') private translate: AbstractTranslateService,
+    private config: NgbPopoverConfig) {
+  }
+
+
+  // ------- ControlValueAccessor -----
+
+  // ControlValueAccessor --> writes a new value from the form model into the view
+  writeValue(value: any) {
+    // console.log('writeValue -> ', value, this.ngInputRef);
+    if (value !== undefined) {
+      this.bfModel = value ? value: '';
+      this.updateStatus();
+      // this.inputCtrl.updateValueAndValidity(value);
+
+      // After one cycle, set the value to the formControl, so the validate() gets the last
+      // state in "inputCtrl.status" when updating ngModel outer link
+      if (!!this.inputCtrl) {
+        setTimeout(() => {
+          this.inputCtrl.setValue(this.bfModel);
+        });
+      }
+    }
+  }
+
+  public propagateModelUp = (_: any) => {}; // This is just to avoid type error (it's overwritten on register)
+  registerOnChange(fn) { this.propagateModelUp = fn; }
+  registerOnTouched(fn) { }
+
+
+  // NG_VALIDATORS ---> outer formControl validation
+  validate(control: FormControl) {
+    // console.log('validate', this.inputCtrl.status, this.ngInputRef.status, this.ngInputRef.value);
+    // control.updateValueAndValidity();
+
+    if (!!this.inputCtrl && this.inputCtrl.status === 'INVALID') {  // If internal ngModel is invalid, external is invalid too
+      // return {'incorrect': true};
+      return { 'required': false };
+      // control.setErrors({ notUnique: true });
+      // control.updateValueAndValidity();
+      // return { notUnique: true };
+    } else {
+      return null;
+    }
+
+    // TODO ---> https://blog.thoughtram.io/angular/2016/03/14/custom-validators-in-angular-2.html
+    // Check there how to do proper custom validators
+  }
+
+  // ------------------------------------
+
+  ngOnChanges(change) { // Translate bfText whenever it changes
+    // console.log('ngOnChanges', change);
+    // console.log('this.ngInputRef', this.ngInputRef);
+
+    // Link the formControl form the <input #ngInputRef="ngModel"> to "this.inputCtrl"
+    if (!!this.ngInputRef['control'] && !this.inputCtrl) {
+      //noinspection TypeScriptUnresolvedVariable
+      this.inputCtrl = this.ngInputRef['control'];
+    }
+
+    if (!!this.translate.doTranslate) {
+      if (!!change.bfLabel)       { this.bfLabelTrans = this.translate.doTranslate(this.bfLabel); }
+      if (!!change.bfTooltip)     { this.bfTooltipTrans = this.translate.doTranslate(this.bfTooltip); }
+      if (!!change.bfPlaceholder) { this.bfPlaceholderTrans = this.translate.doTranslate(this.bfPlaceholder); }
+    } else {
+      if (!!change.bfLabel)       { this.bfLabelTrans = this.bfLabel; }
+      if (!!change.bfTooltip)     { this.bfTooltipTrans = this.bfTooltip; }
+      if (!!change.bfPlaceholder) { this.bfPlaceholderTrans = this.bfPlaceholder; }
+    }
+
+    this.propagateModelUp(this.bfModel);
+    this.updateStatus();
+  }
+
+
+  ngOnInit() { }
+
+  public updateStatus = () => {
+    if (!!this.inputCtrl) {
+      if (this.inputCtrl.pristine) { this.status = 'pristine'; }
+      if (this.inputCtrl.dirty)    { this.status = 'dirty'; }
+
+      // if (this.inputCtrl.status === 'INVALID') { // <--- If we have to show error on pristine
+      if (this.inputCtrl.status === 'INVALID' && !this.inputCtrl.pristine)   {
+        this.status = 'error';
+      }
+    }
+  };
+
+
+  public parseModelChange = (value) => {
+    this.bfModel = value;
+    // this.inputCtrl.updateValueAndValidity();
+    // console.log('propagateModelUp -> ', this.bfModel);
+    this.propagateModelUp(this.bfModel);
+    this.updateStatus();
+    // this.bfModelChange.emit(this.bfModel);
   }
 
 }
