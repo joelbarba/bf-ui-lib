032beea1479397452be5c005db6e02c74e7cf44c
joel <joel.barba@blueface.com>
2019-12-30 14:36:34 +0000

    Add "doc-demos-list" to gather all export classes (documents)

--- projects/bf-ui-lib-sandbox/src/app/app-routing.module.ts
+++ projects/bf-ui-lib-sandbox/src/app/app-routing.module.ts
@@ -2,22 +2,51 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { CommonModule } from '@angular/common';
 import { IndexPageComponent } from './index-page/index-page.component';
-import { compList } from './lib-register.service';
+import * as docs from './doc-demos-list';
+
 
 const routes: Routes = [
   { path: '', redirectTo: '/index', pathMatch: 'full' },
   { path: 'index',  component: IndexPageComponent },
+  { path: docs.BfBtnDoc.name,               component: docs.BfBtnDoc.demoComp              },
+  { path: docs.BfLabelDoc.name,             component: docs.BfLabelDoc.demoComp            },
+  { path: docs.BfInputDoc.name,             component: docs.BfInputDoc.demoComp            },
+  { path: docs.BfTextareaDoc.name,          component: docs.BfTextareaDoc.demoComp         },
+  { path: docs.BfDropdownDoc.name,          component: docs.BfDropdownDoc.demoComp         },
+  { path: docs.BfAutocompleteDoc.name,      component: docs.BfAutocompleteDoc.demoComp     },
+  { path: docs.BfMultiSelectorDoc.name,     component: docs.BfMultiSelectorDoc.demoComp    },
+  { path: docs.BfSwitchDoc.name,            component: docs.BfSwitchDoc.demoComp           },
+  { path: docs.BfCheckboxDoc.name,          component: docs.BfCheckboxDoc.demoComp         },
+  { path: docs.BfRadioDoc.name,             component: docs.BfRadioDoc.demoComp            },
+  { path: docs.BfQuantityInputDoc.name,     component: docs.BfQuantityInputDoc.demoComp    },
+  { path: docs.BfDatePickerDoc.name,        component: docs.BfDatePickerDoc.demoComp       },
+  { path: docs.BfTimePickerDoc.name,        component: docs.BfTimePickerDoc.demoComp       },
+  { path: docs.BfSliderDoc.name,            component: docs.BfSliderDoc.demoComp           },
+  { path: docs.BfColorPickerDoc.name,       component: docs.BfColorPickerDoc.demoComp      },
+  { path: docs.BfStatusBadgeDoc.name,       component: docs.BfStatusBadgeDoc.demoComp      },
+  { path: docs.BfProgressBarDoc.name,       component: docs.BfProgressBarDoc.demoComp      },
+  { path: docs.BfListHeaderColDoc.name,     component: docs.BfListHeaderColDoc.demoComp    },
+  { path: docs.BfListPlaceholderDoc.name,   component: docs.BfListPlaceholderDoc.demoComp  },
+  { path: docs.BfPagePlaceholderDoc.name,   component: docs.BfPagePlaceholderDoc.demoComp  },
+  { path: docs.BfListPaginatorDoc.name,     component: docs.BfListPaginatorDoc.demoComp    },
+  { path: docs.BfNoDataDoc.name,            component: docs.BfNoDataDoc.demoComp           },
+  { path: docs.BfLoadingSpinnerDoc.name,    component: docs.BfLoadingSpinnerDoc.demoComp   },
+  { path: docs.BfGrowlDoc.name,             component: docs.BfGrowlDoc.demoComp            },
+  { path: docs.BfLoadingBarDoc.name,        component: docs.BfLoadingBarDoc.demoComp       },
+  { path: docs.BfConfirmDoc.name,           component: docs.BfConfirmDoc.demoComp          },
+  { path: docs.BfPrototypesDoc.name,        component: docs.BfPrototypesDoc.demoComp       },
+  { path: docs.BfListHandlerDoc.name,       component: docs.BfListHandlerDoc.demoComp      },
+  { path: docs.BfPromiseDoc.name,           component: docs.BfPromiseDoc.demoComp          },
+  { path: docs.BfDeferDoc.name,             component: docs.BfDeferDoc.demoComp            },
+  { path: docs.ShowDoc.name,                component: docs.ShowDoc.demoComp               },
   { path: 'lazy-loading-test',
     loadChildren: () => import('projects/bf-ui-lib-sandbox/src/app/bf-lazy-loaded-module/bf-lazy-loaded.module')
       .then(mod => {
         return mod.BfLazyLoadedModule;
       }),
-  },
- ];
+  }
+];
 
-compList.forEach((item) => {
-   routes.push({ path: item.name,  component: item.demoComp });
-});
 
 @NgModule({
   declarations: [],
--- projects/bf-ui-lib-sandbox/src/app/lib-register.service.ts
+++ projects/bf-ui-lib-sandbox/src/app/lib-register.service.ts
@@ -1,70 +1,40 @@
-import { BfNoDataDoc } from './bf-no-data-demo/bf-no-data-demo.component';
-import { ShowDoc } from './show-demo/show-demo.component';
-import { BfLoadingSpinnerDoc } from './bf-loading-spinner-demo/bf-loading-spinner-demo.component';
-import { BfPagePlaceholderDoc } from './bf-page-placeholder-demo/bf-page-placeholder-demo.component';
-import { BfSliderDoc } from './bf-slider-demo/bf-slider-demo.component';
-import { BfColorPickerDoc } from './bf-color-picker-demo/bf-color-picker-demo.component';
-import { BfStatusBadgeDoc } from './bf-status-badge-demo/bf-status-badge-demo.component';
-import { BfProgressBarDoc } from './bf-progress-bar-demo/bf-progress-bar-demo.component';
-import { BfMultiSelectorDoc } from './bf-multi-selector-demo/bf-multi-selector-demo.component';
-import { BfAutocompleteDoc } from './bf-autocomplete-demo/bf-autocomplete-demo.component';
-import { BfTimePickerDoc } from './bf-time-picker-demo/bf-time-picker-demo.component';
-import { BfDeferDoc } from './bf-defer-demo/bf-defer-demo.component';
-import { BfPromiseDoc } from './bf-promise-demo/bf-promise-demo.component';
-import { BfLoadingBarDoc } from './bf-loading-bar-demo/bf-loading-bar-demo.component';
-import { BfPrototypesDoc } from './bf-prototypes-demo/bf-prototypes-demo.component';
-import { BfListPaginatorDoc } from './bf-list-paginator-demo/bf-list-paginator-demo.component';
-import { BfDatePickerDoc } from './bf-date-picker-demo/bf-date-picker-demo.component';
-import { BfRadioDoc } from './bf-radio-demo/bf-radio-demo.component';
-import { BfTextareaDoc } from './bf-textarea-demo/bf-textarea-demo.component';
-import { BfConfirmDoc } from './bf-confirm-demo/bf-confirm-demo.component';
-import { BfQuantityInputDoc } from './bf-quantity-input-demo/bf-quantity-input-demo.component';
-import { BfSwitchDoc }          from './bf-switch-demo/bf-switch-demo.component';
-import { BfDropdownDoc }        from './bf-dropdown-demo/bf-dropdown-demo.component';
-import { BfInputDoc }           from './bf-input-demo/bf-input-demo.component';
-import { BfCheckboxDoc }        from './bf-checkbox-demo/bf-checkbox-demo.component';
-import { BfListPlaceholderDoc } from './bf-list-placeholder-demo/bf-list-placeholder-demo.component';
-import { BfLabelDoc }           from './bf-label-demo/bf-label-demo.component';
-import { BfBtnDoc }             from './bf-btn-demo/bf-btn-demo.component';
-import { BfListHeaderColDoc }   from './bf-list-header-col-demo/bf-list-header-col-demo.component';
-import { BfGrowlDoc }           from './bf-growl-demo/bf-growl-demo.component';
-import { BfListHandlerDoc } from './bf-list-handler-demo/bf-list-handler-demo.component';
 import { Injectable } from '@angular/core';
-import { Router } from '@angular/router';
-
+import * as docs from './doc-demos-list';
 export const compList = [
-  BfBtnDoc,
-  BfLabelDoc,
-  BfInputDoc,
-  BfTextareaDoc,
-  BfDropdownDoc,
-  BfAutocompleteDoc,
-  BfMultiSelectorDoc,
-  BfSwitchDoc,
-  BfCheckboxDoc,
-  BfRadioDoc,
-  BfQuantityInputDoc,
-  BfDatePickerDoc,
-  BfTimePickerDoc,
-  BfSliderDoc,
-  BfColorPickerDoc,
-  BfStatusBadgeDoc,
-  BfProgressBarDoc,
-  BfListHeaderColDoc,
-  BfListPlaceholderDoc,
-  BfPagePlaceholderDoc,
-  BfListPaginatorDoc,
-  BfNoDataDoc,
-  BfLoadingSpinnerDoc,
-  BfGrowlDoc,
-  BfLoadingBarDoc,
-  BfConfirmDoc,
-  BfPrototypesDoc,
-  BfListHandlerDoc,
-  BfPromiseDoc,
-  BfDeferDoc,
-  ShowDoc,
+  docs.BfBtnDoc,
+  docs.BfLabelDoc,
+  docs.BfInputDoc,
+  docs.BfTextareaDoc,
+  docs.BfDropdownDoc,
+  docs.BfAutocompleteDoc,
+  docs.BfMultiSelectorDoc,
+  docs.BfSwitchDoc,
+  docs.BfCheckboxDoc,
+  docs.BfRadioDoc,
+  docs.BfQuantityInputDoc,
+  docs.BfDatePickerDoc,
+  docs.BfTimePickerDoc,
+  docs.BfSliderDoc,
+  docs.BfColorPickerDoc,
+  docs.BfStatusBadgeDoc,
+  docs.BfProgressBarDoc,
+  docs.BfListHeaderColDoc,
+  docs.BfListPlaceholderDoc,
+  docs.BfPagePlaceholderDoc,
+  docs.BfListPaginatorDoc,
+  docs.BfNoDataDoc,
+  docs.BfLoadingSpinnerDoc,
+  docs.BfGrowlDoc,
+  docs.BfLoadingBarDoc,
+  docs.BfConfirmDoc,
+  docs.BfPrototypesDoc,
+  docs.BfListHandlerDoc,
+  docs.BfPromiseDoc,
+  docs.BfDeferDoc,
+  docs.ShowDoc,
 ];
+
+
 
 /* {
   name     : 'bf-list-header-col',
@@ -73,11 +43,8 @@ export const compList = [
   demoComp : BfBtnDemoComponent
 } */
 
-@Injectable({
-  providedIn: 'root'
-})
+@Injectable({ providedIn: 'root' })
 export class LibRegisterService {
   public compList = compList;
-
   constructor() {  }
 }
