af0e89eb82a9033930754943113e1ffda7a833f2
joel <joel.barba@blueface.com>
2020-01-16 17:05:46 +0000

    Update docs

--- projects/bf-ui-lib-sandbox/src/app/index-page/index-page.component.ts
+++ projects/bf-ui-lib-sandbox/src/app/index-page/index-page.component.ts
@@ -7,20 +7,48 @@ import { LibRegisterService } from '../lib-register.service';
   styleUrls: ['./index-page.component.scss']
 })
 export class IndexPageComponent implements OnInit {
-  public libImport = `@NgModule({
-  ...
-  BfUiLibModule.forRoot({ trans: {
-      useExisting: BfTranslateService
-    }
-  }),
-})`;
+  public libImport = `npm i @blueface_npm/bf-ui-lib@0.4.9
+npm i @blueface_npm/bf-icomoon@1.0.3
+npm i bootstrap@4.4.1
+npm i @ng-bootstrap/ng-bootstrap@5.1.5
+
+-- For Angular >= 9, you also need:
+ng add @angular/localize`;
+
+  public libImport2 = `import {FormsModule} from "@angular/forms";
+import {BfUiLibModule} from "@blueface_npm/bf-ui-lib";
+
+@Injectable({ providedIn: 'root' })
+export class BfTranslateService {
+  public onLangChange$ = new BehaviorSubject({lang: '', translations: []});
+  public doTranslate = (label ?: string, params?): string => label;
+  public getLabel$ = (label ?: string, params?) => of(label);
+}
+
+@NgModule({
+  declarations: [AppComponent],
+  imports: [
+    BrowserModule,
+    FormsModule,
+    BfUiLibModule.forRoot({ trans: {
+        useExisting: BfTranslateService
+      }
+    }),
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }`;
+
+
+
 
   public stylesExample = `@import "scss/variables.scss";
-@import 'bf-ui-lib/scss/index';
 
-@import "~bootstrap/scss/bootstrap.scss";
-@import "~bf-icomoon/css/icomoon.css";
-...  `;
+@import '~@blueface_npm/bf-ui-lib/scss/index';
+
+@import "~@blueface_npm/bf-icomoon/css/icomoon.css";
+@import "~bootstrap/scss/bootstrap.scss";`;
 
   constructor(private reg: LibRegisterService) { }
   ngOnInit() { }
