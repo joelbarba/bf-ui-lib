f2d44f32571b61a7eeb8affe56a91ac7b346ef90
Alvaro Patty <alvaro.patty@blueface.com>
2020-03-03 15:38:54 +0000

    SPL-2157 Tests added

--- projects/bf-ui-lib/src/lib/bf-slider/bf-slider.component.spec.ts
+++ projects/bf-ui-lib/src/lib/bf-slider/bf-slider.component.spec.ts
@@ -1,6 +1,9 @@
 import { async, ComponentFixture, TestBed } from '@angular/core/testing';
 
 import { BfSliderComponent } from './bf-slider.component';
+import {BfLabelComponent} from '../bf-label/bf-label.component';
+import {Ng5SliderModule, Options} from 'ng5-slider';
+import {NgbTooltipModule} from '@ng-bootstrap/ng-bootstrap';
 
 describe('BfSliderComponent', () => {
   let component: BfSliderComponent;
@@ -8,7 +11,8 @@ describe('BfSliderComponent', () => {
 
   beforeEach(async(() => {
     TestBed.configureTestingModule({
-      declarations: [ BfSliderComponent ]
+      declarations: [ BfSliderComponent, BfLabelComponent ],
+      imports: [Ng5SliderModule, NgbTooltipModule]
     })
     .compileComponents();
   }));
@@ -16,10 +20,72 @@ describe('BfSliderComponent', () => {
   beforeEach(() => {
     fixture = TestBed.createComponent(BfSliderComponent);
     component = fixture.componentInstance;
+    component.bfOptions = { start: 10, end: 150 };
     fixture.detectChanges();
   });
 
-  it('should create', () => {
+  it('Should create', () => {
     expect(component).toBeTruthy();
+  });
+
+  it('Should customize bfOptions', () => {
+    component.bfOptions = {
+      start: 100,
+      end: 150,
+      step: 2,
+      showTicks: true,
+      showTicksValues: true,
+      tickStep: 10,
+      tickValueStep: 5
+    };
+    component.ngOnInit();
+    expect(component.sliderOptions).toEqual({
+      animate: false,
+      floor: 100,
+      ceil: 150,
+      disabled: false,
+      step: 2,
+      showTicks: true,
+      showTicksValues: true,
+      tickStep: 10,
+      tickValueStep: 5,
+      ticksArray: null,
+      showOuterSelectionBars: false
+    });
+  });
+
+  it('Should add ticksArray', () => {
+    component.bfOptions = {
+      start: 100,
+      end: 150,
+      showTicks: true,
+      showTicksValues: true,
+      tickArray: [100, 110, 120, 130, 140, 150]
+    };
+    component.optionsRebuild();
+    expect(component.sliderOptions.floor).toBe(100);
+    expect(component.sliderOptions.ceil).toBe(150);
+    expect(component.sliderOptions.ticksArray).toEqual([100, 110, 120, 130, 140, 150]);
+  });
+
+  it('Should Disabled the Slider', () => {
+    component.bfDisabled = true;
+    component.ngOnChanges({
+      bfDisabled: { currentValue: true }
+    });
+    expect(component.sliderOptions.disabled).toBe(true);
+  });
+
+  it('Should Show outer Selection', () => {
+    component.bfHighValue = 120;
+    component.bfShowOuterSection = true;
+    component.ngOnInit();
+    expect(component.sliderOptions.showOuterSelectionBars).toBe(true);
+  });
+
+  it('Should update bfHighValue', () => {
+    component.highValue = 70;
+    component.onChange();
+    expect(component.bfHighValue).toBe(70);
   });
 });
