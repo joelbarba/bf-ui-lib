92ce141d3902d41ea2ea2353bc118dbe916400ef
Magali Platero <magali.platero@blueface.com>
2020-03-27 10:12:06 +0000

    SPL-2789 - Change pipe to translate text

--- projects/bf-ui-lib/src/lib/bf-section-header/bf-section-header.component.spec.ts
+++ projects/bf-ui-lib/src/lib/bf-section-header/bf-section-header.component.spec.ts
@@ -1,19 +1,17 @@
 import { async, ComponentFixture, TestBed } from '@angular/core/testing';
 
 import { BfSectionHeaderComponent } from './bf-section-header.component';
-import { BfUILibTransService } from '../abstract-translate.service';
+import { BfUILibTransService, BfTranslatePipe } from '../abstract-translate.service';
 import { BfUILibTransStubService } from '../../testing/bf-ui-lib-trans-service-stub.service';
-import { of } from 'rxjs';
 import { By } from '@angular/platform-browser';
 
 describe('BfSectionHeaderComponent', () => {
   let component: BfSectionHeaderComponent;
   let fixture: ComponentFixture<BfSectionHeaderComponent>;
-  let translateService: BfUILibTransStubService;
 
   beforeEach(async(() => {
     TestBed.configureTestingModule({
-      declarations: [ BfSectionHeaderComponent ],
+      declarations: [ BfSectionHeaderComponent, BfTranslatePipe ],
       providers: [{ provide: BfUILibTransService, useClass: BfUILibTransStubService}]
     })
     .compileComponents();
@@ -22,50 +20,11 @@ describe('BfSectionHeaderComponent', () => {
   beforeEach(() => {
     fixture = TestBed.createComponent(BfSectionHeaderComponent);
     component = fixture.componentInstance;
-    translateService = TestBed.get(BfUILibTransService);
     fixture.detectChanges();
   });
 
   it('should create', () => {
     expect(component).toBeTruthy();
-  });
-
-  it('should show the title', () => {
-    const title = 'Title';
-    component.bfTitle = 'view.common.title';
-    const getLabelsSpy = spyOn(translateService, 'getLabel$').and.returnValue(of(title));
-
-    component.ngOnChanges();
-    fixture.detectChanges();
-
-    const titleElement = fixture.debugElement.query(By.css('.section-header--title')).nativeElement;
-
-    expect(titleElement.innerText).toBe(title);
-    expect(getLabelsSpy).toHaveBeenCalledWith(component.bfTitle);
-  });
-
-  it('should show the description', () => {
-    const description = 'Description';
-    component.bfDescription = 'view.common.description';
-    const getLabelsSpy = spyOn(translateService, 'getLabel$').and.returnValue(of(description));
-
-    component.ngOnChanges();
-    fixture.detectChanges();
-
-    const descriptionElement = fixture.debugElement.query(By.css('.section-header--description')).nativeElement;
-
-    expect(descriptionElement.innerText).toBe(description);
-    expect(getLabelsSpy).toHaveBeenCalledWith(component.bfDescription);
-  });
-
-  it('should not show the description', () => {
-    component.bfDescription = null;
-    const getLabelsSpy = spyOn(translateService, 'getLabel$');
-
-    const descriptionElement = fixture.debugElement.query(By.css('.section-header--description'));
-
-    expect(getLabelsSpy).not.toHaveBeenCalled();
-    expect(descriptionElement).toBeFalsy();
   });
 
 });
--- projects/bf-ui-lib/src/lib/bf-section-header/bf-section-header.component.ts
+++ projects/bf-ui-lib/src/lib/bf-section-header/bf-section-header.component.ts
@@ -1,35 +1,16 @@
-import { Component, OnInit, Input, OnChanges } from '@angular/core';
-import { Observable, of } from 'rxjs';
-import { BfUILibTransService } from '../abstract-translate.service';
+import { Component, OnInit, Input } from '@angular/core';
 
 @Component({
   selector: 'bf-section-header',
   templateUrl: './bf-section-header.component.html'
 })
-export class BfSectionHeaderComponent implements OnInit, OnChanges {
+export class BfSectionHeaderComponent implements OnInit {
   @Input() bfTitle: string;
   @Input() bfDescription: string = null;
 
-  public bfCurrentTitle$: Observable<string> = of('');
-  public bfCurrentDescription$: Observable<string> = of('');
-
-  constructor(private translate: BfUILibTransService) { }
+  constructor() { }
 
   ngOnInit() {
-  }
-
-  ngOnChanges() {
-    this.setLabel();
-  }
-
-  setLabel = () => {
-    if (this.bfTitle) {
-      this.bfCurrentTitle$ = this.translate.getLabel$(this.bfTitle);
-    }
-
-    if (this.bfDescription) {
-      this.bfCurrentDescription$ = this.translate.getLabel$(this.bfDescription);
-    }
   }
 
 }
