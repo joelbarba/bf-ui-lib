8811d33492d87b3970ea620a00facbca396c918a
Andrew Byrne <andrew.byrne@blueface.com>
2020-12-07 13:44:02 +0000

    SPL-4202 aria label on async button

--- projects/bf-ui-lib-sandbox/src/app/accessibility-demo/a11y-form/a11y-form.component.ts
+++ projects/bf-ui-lib-sandbox/src/app/accessibility-demo/a11y-form/a11y-form.component.ts
@@ -10,6 +10,9 @@ export class A11yFormComponent implements OnInit {
   public item: any = {};
   public list = [{id: '1'},{id: '2'},{id: '3'},{id: '4'}];
   public buttonClicked = false;
+  public blockPr;
+  public bfAsyncAriaLabel;
+
   constructor() { }
 
   ngOnInit(): void {
@@ -17,6 +20,16 @@ export class A11yFormComponent implements OnInit {
 
   clickButton() {
     console.log('click button');
+  }
+
+  public asyncClick() {
+    this.bfAsyncAriaLabel = 'Fetching Data'
+    return this.blockPr = new Promise((resolve) => {
+      setTimeout(() => {
+        resolve();
+        this.bfAsyncAriaLabel = null;
+      }, 5000);
+    });
   }
 
 }
