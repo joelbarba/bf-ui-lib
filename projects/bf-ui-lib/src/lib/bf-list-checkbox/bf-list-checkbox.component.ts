91e8a4b7d9005084d00a4315a2247423431159d8
Andrew Byrne <andrew.byrne@blueface.com>
2021-01-13 14:45:32 +0000

    SPl-4185 wip table navigation

--- projects/bf-ui-lib/src/lib/bf-list-checkbox/bf-list-checkbox.component.ts
+++ projects/bf-ui-lib/src/lib/bf-list-checkbox/bf-list-checkbox.component.ts
@@ -1,11 +1,11 @@
-import {Component, EventEmitter, Input, OnChanges, OnDestroy, OnInit, Output} from '@angular/core';
+import {Component, ElementRef, EventEmitter, Input, OnChanges, OnDestroy, Output} from '@angular/core';
 import {BfListSelection} from '../bf-list-selection/bf-list-selection';
 
 @Component({
   selector: 'bf-list-checkbox',
   templateUrl: './bf-list-checkbox.component.html',
 })
-export class BfListCheckboxComponent implements OnInit, OnChanges, OnDestroy {
+export class BfListCheckboxComponent implements OnChanges, OnDestroy {
   @Input() selection: BfListSelection;
   @Input() id: string;
   @Input() bfDisabled = false;
@@ -17,9 +17,7 @@ export class BfListCheckboxComponent implements OnInit, OnChanges, OnDestroy {
   public hasId = false;
   public actionsExp = false;
 
-  constructor() { }
-
-  ngOnInit() { }
+  constructor(public elementRef: ElementRef) { }
 
   ngOnChanges(changes) {
     this.hasId = this.id !== undefined;
