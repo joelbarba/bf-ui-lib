ccf0b3e8279f5a3dde6764281efe25b27ee4f7b5
Chris McNulty <christopher.mcnulty@blueface.com>
2021-10-14 10:10:08 +0100

    SPL-5357 bf-quanity-input a11y changes

--- projects/bf-ui-lib/src/lib/bf-quantity-input/bf-quantity-input.component.spec.ts
+++ projects/bf-ui-lib/src/lib/bf-quantity-input/bf-quantity-input.component.spec.ts
@@ -1,9 +1,10 @@
-import { ComponentFixture, TestBed, waitForAsync } from '@angular/core/testing';
+import { ComponentFixture, fakeAsync, flush, TestBed, waitForAsync } from '@angular/core/testing';
 import { FormControlDirective, FormsModule } from '@angular/forms';
 
 import { BfQuantityInputComponent } from './bf-quantity-input.component';
 import { TestingModule } from '../../testing/testing-module';
 import {SimpleChange} from '@angular/core';
+import { BfUILibTransService } from '../abstract-translate.service';
 
 describe('BfQuantityInputComponent', () => {
   let component: BfQuantityInputComponent;
@@ -33,4 +34,85 @@ describe('BfQuantityInputComponent', () => {
     fixture.detectChanges();
     expect(fixture.nativeElement.querySelector('label span').textContent).toEqual('Test');
   });
+
+  it('should return a validation error if current value is below the minimum', () => {
+    const result = component.getValidationError(100, 120, 150);
+    expect(result).toEqual({ label: 'components.bf_quantity_input.min_value.error', params: { min: 120 } });
+  });
+
+  it('should return a validation error if current value is above the maximum', () => {
+    const result = component.getValidationError(160, 120, 150);
+    expect(result).toEqual({ label: 'components.bf_quantity_input.max_value.error', params: { max: 150 } });
+  });
+
+  it('should return not error if the current value is within the limits of max and min', () => {
+    const result = component.getValidationError(100, 80, 160);
+    expect(result).toBeNull();
+  });
+
+  it('should set the value of the bfErrorTrans$ to the error returned from the validation function', fakeAsync(() => {
+    spyOn(component, 'getValidationError').and.returnValue({ label: 'components.bf_quantity_input.max_value.error', params: { max: 150 } });
+    spyOn(TestBed.inject(BfUILibTransService), 'doTranslate').and.callFake(() => 'Value cannot be greater than 150');
+
+    component.validateValue(160);
+    flush();
+
+    component.bfErrorTrans$.asObservable().subscribe((value) => {
+      expect(value).toBe('Value cannot be greater than 150');
+    });
+  }));
+
+  it('should set the value of bfErrorTrans$ to the error returned from the validation function', fakeAsync(() => {
+    spyOn(component, 'getValidationError').and.returnValue(null);
+    component.validateValue(160);
+    flush();
+
+    component.bfErrorTrans$.asObservable().subscribe((value) => {
+      expect(value).toBe('');
+    });
+  }));
+
+  it('should only increment the value if the increment buttons are enabled', fakeAsync(() => {
+    component.incBtnEnabled = true;
+    fixture.detectChanges();
+
+    component.incrementValue(12);
+    fixture.detectChanges();
+    flush();
+
+    expect(component.bfModel).toEqual(13);
+  }));
+
+  it('should not increment the value if the increment buttons are disabled', fakeAsync(() => {
+    component.incBtnEnabled = false;
+    fixture.detectChanges();
+
+    component.incrementValue(12);
+    fixture.detectChanges();
+    flush();
+
+    expect(component.bfModel).toEqual(0);
+  }));
+
+  it('should only decrement the value if the decrement buttons are enabled', fakeAsync(() => {
+    component.decBtnEnabled = true;
+    fixture.detectChanges();
+
+    component.decrementValue(12);
+    fixture.detectChanges();
+    flush();
+
+    expect(component.bfModel).toEqual(11);
+  }));
+
+  it('should not decrement the value if the decrement buttons are disabled', fakeAsync(() => {
+    component.decBtnEnabled = false;
+    fixture.detectChanges();
+
+    component.decrementValue(12);
+    fixture.detectChanges();
+    flush();
+
+    expect(component.bfModel).toEqual(0);
+  }));
 });
