9ce9092fa94abfa8c13ef1401661c0ef6ee62887
joel <joel.barba@blueface.com>
2022-05-05 12:49:46 +0200

    SPL-5361 - BfUiLib - Add a bf-radio-group component to wrap bf-radio Add some tests

--- projects/bf-ui-lib/src/lib/bf-radio-group/bf-radio-group.component.spec.ts
+++ projects/bf-ui-lib/src/lib/bf-radio-group/bf-radio-group.component.spec.ts
@@ -1,73 +1,47 @@
-// import { Component, DebugElement } from '@angular/core';
-// import { ComponentFixture, TestBed } from '@angular/core/testing';
-// import { By } from '@angular/platform-browser';
-// import { FormsModule } from '@angular/forms';
-// import { NgbTooltipModule } from '@ng-bootstrap/ng-bootstrap';
-// import { TestingModule } from '../../testing/testing-module';
-//
-//
-// @Component({
-//   template: `
-//     <form #form="ngForm" class="padT25">
-//       <bf-radio [class.block]="compConf.isClassBlock"
-//                 [style.background]="compConf.isClassBlock ? 'lightgray' : 'white'"
-//                 [(ngModel)]="myVariable" name="radio"
-//                 [bfValue]="compConf.value"
-//                 [bfLabel]="compConf.labelText"
-//                 [bfRadioGroup]="compConf.hasGroup? compConf.groupValue : ''"
-//                 [bfDisabled]="compConf.isDisabled"
-//                 [bfRequired]="compConf.isRequired"
-//                 [bfTooltip]="(compConf.hasTooltip ? compConf.tooltipText : null)"
-//                 [bfTooltipPos]="(compConf.tooltipPos || 'top')"
-//                 [bfTooltipBody]="compConf.tooltipBody"
-//       ></bf-radio>
-//     </form>
-//   `
-// })
-// export class TestComponent {
-//   myVariable: string;
-//   compConf = {
-//     value: '1',
-//     labelText: 'view.common.name',
-//     hasGroup: false, groupValue: 'radio-group-1',
-//     isRequired: true, isDisabled: false,
-//     hasTooltip: false, tooltipText: 'view.tooltip.message', tooltipPos: null, tooltipBody: 'true',
-//     isClassBlock: false,
-//   };
-// }
-//
-//
-// describe('BfRadioGroupComponent', () => {
-//   let comp: TestComponent;
-//   let fixture: ComponentFixture<TestComponent>;
-//   let debugElement: DebugElement;
-//   let form: HTMLFormElement;
-//   let radioBtn: HTMLFormElement;
-//
-//   beforeEach(async () => {
-//     await TestBed.configureTestingModule({
-//       declarations: [BfRadioGroupComponent, TestComponent],
-//       imports: [TestingModule, FormsModule, NgbTooltipModule],
-//     }).compileComponents();
-//
-//     fixture = TestBed.createComponent(TestComponent);
-//     debugElement = fixture.debugElement;
-//     comp = fixture.componentInstance;
-//     form = debugElement.query(By.css('form')).nativeElement;
-//     radioBtn = debugElement.query(By.css('bf-radio')).nativeElement;
-//     fixture.detectChanges();
-//   });
-//
-//
-//   describe('INIT', () => {
-//     it('should create the component', () => {
-//       expect(comp).toBeTruthy();
-//     });
-//   });
-//
-//   describe('tabindex', () => {
-//     it('should set a tabindex value of the a11y features are disabled', () => {
-//       expect(radioBtn.tabIndex).toEqual(0);
-//     });
-//   });
-// });
+import {ComponentFixture, TestBed, waitForAsync} from '@angular/core/testing';
+import { NgbTooltipModule } from '@ng-bootstrap/ng-bootstrap';
+import { TestingModule } from '../../testing/testing-module';
+import {BfRadioGroupComponent} from "./bf-radio-group.component";
+
+
+describe('BfRadioGroupComponent', () => {
+  let comp: BfRadioGroupComponent;
+  let fixture: ComponentFixture<BfRadioGroupComponent>;
+
+  beforeEach(waitForAsync(() => {
+    TestBed.configureTestingModule({
+      declarations: [ BfRadioGroupComponent ],
+      imports: [ TestingModule, NgbTooltipModule ],
+    }).compileComponents();
+
+    fixture = TestBed.createComponent(BfRadioGroupComponent);
+    comp = fixture.componentInstance;
+    fixture.detectChanges();
+  }));
+
+  describe('internalChange', () => {
+    beforeEach(() => {
+      spyOn(comp, 'updateTabIndexes');
+      spyOn(comp, 'propagateModelUp');
+    });
+    it('should set the model', () => {
+      comp.internalChange('123');
+      expect(comp.bfModel).toEqual('123');
+    });
+    it('should set pristine false', () => {
+      comp.internalChange('123');
+      expect(comp.isPristine).toEqual(false);
+    });
+    it('should do nothing if setting the same value', () => {
+      comp.bfModel = '123';
+      comp.internalChange('123');
+      expect(comp.isPristine).toEqual(true);
+    });
+    it('should update tab indexes and propagate', () => {
+      comp.internalChange('123');
+      expect(comp.updateTabIndexes).toHaveBeenCalled();
+      expect(comp.propagateModelUp).toHaveBeenCalledWith('123');
+    });
+  });
+
+});
--- projects/bf-ui-lib/src/lib/bf-radio/bf-radio.component.spec.ts
+++ projects/bf-ui-lib/src/lib/bf-radio/bf-radio.component.spec.ts
@@ -1,75 +1,90 @@
-// import { Component, DebugElement } from '@angular/core';
-// import { ComponentFixture, TestBed } from '@angular/core/testing';
-// import { By } from '@angular/platform-browser';
-// import { FormsModule } from '@angular/forms';
-// import { NgbTooltipModule } from '@ng-bootstrap/ng-bootstrap';
-// import { TestingModule } from '../../testing/testing-module';
-//
-// import { BfRadioComponent } from './bf-radio.component';
-//
-//
-// @Component({
-//   template: `
-//     <form #form="ngForm" class="padT25">
-//       <bf-radio [class.block]="compConf.isClassBlock"
-//                 [style.background]="compConf.isClassBlock ? 'lightgray' : 'white'"
-//                 [(ngModel)]="myVariable" name="radio"
-//                 [bfValue]="compConf.value"
-//                 [bfLabel]="compConf.labelText"
-//                 [bfRadioGroup]="compConf.hasGroup? compConf.groupValue : ''"
-//                 [bfDisabled]="compConf.isDisabled"
-//                 [bfRequired]="compConf.isRequired"
-//                 [bfTooltip]="(compConf.hasTooltip ? compConf.tooltipText : null)"
-//                 [bfTooltipPos]="(compConf.tooltipPos || 'top')"
-//                 [bfTooltipBody]="compConf.tooltipBody"
-//       ></bf-radio>
-//     </form>
-//   `
-// })
-// export class TestComponent {
-//   myVariable: string;
-//   compConf = {
-//     value: '1',
-//     labelText: 'view.common.name',
-//     hasGroup: false, groupValue: 'radio-group-1',
-//     isRequired: true, isDisabled: false,
-//     hasTooltip: false, tooltipText: 'view.tooltip.message', tooltipPos: null, tooltipBody: 'true',
-//     isClassBlock: false,
-//   };
-// }
-//
-//
-// describe('BfRadioComponent', () => {
-//   let comp: TestComponent;
-//   let fixture: ComponentFixture<TestComponent>;
-//   let debugElement: DebugElement;
-//   let form: HTMLFormElement;
-//   let radioBtn: HTMLFormElement;
-//
-//   beforeEach(async () => {
-//     await TestBed.configureTestingModule({
-//       declarations: [BfRadioComponent, TestComponent],
-//       imports: [TestingModule, FormsModule, NgbTooltipModule],
-//     }).compileComponents();
-//
-//     fixture = TestBed.createComponent(TestComponent);
-//     debugElement = fixture.debugElement;
-//     comp = fixture.componentInstance;
-//     form = debugElement.query(By.css('form')).nativeElement;
-//     radioBtn = debugElement.query(By.css('bf-radio')).nativeElement;
-//     fixture.detectChanges();
-//   });
-//
-//
-//   describe('INIT', () => {
-//     it('should create the component', () => {
-//       expect(comp).toBeTruthy();
-//     });
-//   });
-//
-//   describe('tabindex', () => {
-//     it('should set a tabindex value of the a11y features are disabled', () => {
-//       expect(radioBtn.tabIndex).toEqual(0);
-//     });
-//   });
-// });
+import { BfRadioComponent } from './bf-radio.component';
+import { ElementRef, SimpleChange } from "@angular/core";
+
+describe('BfRadioComponent', () => {
+  let comp: BfRadioComponent;
+
+  beforeEach(async () => {
+    comp = new BfRadioComponent();
+    const nativeEl = document.createElement('input') as HTMLInputElement;
+    comp.radioInput = new ElementRef(nativeEl);
+  });
+
+  describe('INIT', () => {
+    it('should create the component', () => {
+      expect(comp).toBeTruthy();
+    });
+  });
+
+  describe('ngOnChanges', () => {
+    it('should set datValue and emit when bfValue changes', () => {
+      comp.bfValue = 123;
+      let result: number;
+      const sub = comp.value$.subscribe(val => result = val);
+      comp.ngOnChanges({ bfValue: new SimpleChange(null, comp.bfValue, true) });
+      expect(comp.dataValue).toEqual('123');
+      expect(result).toEqual(123);
+      sub.unsubscribe();
+    });
+  });
+
+  describe('isDisabled', () => {
+    it('should be disabled if bfDisabled', () => {
+      comp.bfDisabled = true;
+      expect(comp.isDisabled()).toEqual(true);
+    });
+    it('should be disabled if groupDisabled', () => {
+      comp.groupDisabled = true;
+      expect(comp.isDisabled()).toEqual(true);
+    });
+    it('should be enabled when none', () => {
+      expect(comp.isDisabled()).toEqual(false);
+    });
+  });
+
+  describe('internalChange', () => {
+    it('should do nothing if disabled', () => {
+      comp.bfDisabled = true;
+      comp.internalChange('123');
+      expect(comp.bfModel).toEqual(undefined);
+    });
+    it('should set the model', () => {
+      comp.internalChange('123');
+      expect(comp.bfModel).toEqual('123');
+    });
+    it('should check the input if value match', () => {
+      comp.bfValue = '123';
+      comp.internalChange('123');
+      expect(comp.radioInput.nativeElement.checked).toEqual(true);
+    });
+    it('should emit bfOnSelected if value match', () => {
+      let result;
+      const sub = comp.bfOnSelected.subscribe(val => result = val);
+      comp.bfValue = '123';
+      comp.internalChange('123');
+      expect(result).toEqual('123');
+      sub.unsubscribe();
+    });
+  });
+
+  describe('externalChange', () => {
+    it('should set the model', () => {
+      comp.externalChange('123');
+      expect(comp.bfModel).toEqual('123');
+    });
+    it('should check the input if value match', () => {
+      comp.bfValue = '123';
+      comp.externalChange('123');
+      expect(comp.radioInput.nativeElement.checked).toEqual(true);
+    });
+    it('should emit bfOnSelected if value match', () => {
+      let result;
+      const sub = comp.bfOnSelected.subscribe(val => result = val);
+      comp.bfValue = '123';
+      comp.externalChange('123');
+      expect(result).toEqual('123');
+      sub.unsubscribe();
+    });
+  });
+
+});
