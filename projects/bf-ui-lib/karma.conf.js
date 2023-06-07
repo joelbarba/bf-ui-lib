96b6966f4298f63eb36671728236701dc5147d7f
Brennan <bbuite809@emea.comcast.com>
2023-06-07 10:37:34 +0100

    chore: update pipelines
    Refs: TOS-506

--- projects/bf-ui-lib/karma.conf.js
+++ projects/bf-ui-lib/karma.conf.js
@@ -1,6 +1,8 @@
 // Karma configuration file, see link for more information
 // https://karma-runner.github.io/1.0/config/configuration-file.html
 
+process.env.CHROME_BIN = require('puppeteer').executablePath()
+
 const { COVERAGE_LIMIT_STATEMENTS, COVERAGE_LIMIT_BRANCHES, COVERAGE_LIMIT_LINES, COVERAGE_LIMIT_FUNCTIONS } = process.env;
 
 const coverageThresholdLimits = {
